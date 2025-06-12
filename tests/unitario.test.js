39870824ff8c04e961b2cd895b8a57a164ce3b63
DeoclecioSpielmann1 <expedicao-fapolpa2@hotmail.com>
2025-06-11 21:53:20 -0300

    dsp1

--- tests/unitario.test.js
+++ tests/unitario.test.js
@@ -1,14 +1,16 @@
-// Função que roda todos os testes
-function runUnitTests() {
-  // Testes de soma
-  testar("UNIT - Soma de positivos", () => {
-    if (somar(3, 4) !== 7) throw new Error("Esperado 7");
-  });
-
-  testar("UNIT - Soma com negativo", () => {
-    if (somar(5, -2) !== 3) throw new Error("Esperado 3");
-  });
-
-
+  testar("UNIT - alugarVeiculo atualiza mensagem corretamente e limpa após 5s", () => {
   
-}
+            const testId = 'msg-Teste-Carro';
+            const divTeste = document.createElement('div');
+            divTeste.id = testId;
+            document.body.appendChild(divTeste);
+
+            alugarVeiculo('Teste-Carro');
+
+            if (divTeste.textContent !== 'Você alugou o veículo: Teste Carro') {
+                throw new Error(`Mensagem incorreta: ${divTeste.textContent}`);
+            }
+
+            setTimeout(() => {
+                if (divTeste.textContent !== '') {
+                    throw new Error('Mensagem não foi limpa após 5 segundos');
