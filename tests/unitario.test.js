e02e04775ef0aea9b3ce37788e30431cc7fec79f
DeoclecioSpielmann1 <expedicao-fapolpa2@hotmail.com>
2025-06-11 21:55:49 -0300

    dsp1

--- tests/unitario.test.js
+++ tests/unitario.test.js
@@ -1,16 +1,7 @@
+   testar("UNIT - Soma com negativo", () => {
+    if (somar(5, -2) !== 3) throw new Error("Esperado 3");
+  });
   testar("UNIT - alugarVeiculo atualiza mensagem corretamente e limpa após 5s", () => {
-  
-            const testId = 'msg-Teste-Carro';
-            const divTeste = document.createElement('div');
-            divTeste.id = testId;
-            document.body.appendChild(divTeste);
-
-            alugarVeiculo('Teste-Carro');
-
-            if (divTeste.textContent !== 'Você alugou o veículo: Teste Carro') {
+              if (divTeste.textContent !== 'Você alugou o veículo: Teste Carro') {
                 throw new Error(`Mensagem incorreta: ${divTeste.textContent}`);
-            }
-
-            setTimeout(() => {
-                if (divTeste.textContent !== '') {
-                    throw new Error('Mensagem não foi limpa após 5 segundos');
+          
